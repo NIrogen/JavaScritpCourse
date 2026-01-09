15ee887dae2ce249a134927d4870f59bea7ee910
NIrogen <prirporitien@gmail.com>
2026-01-09 12:12:34 +0300

    add: changes

--- lesson4/main4.js
+++ lesson4/main4.js
@@ -1,75 +1,54 @@
-'use strict'
-let title = prompt('как называется ваш проект?', 'калькулятор верстки');
-let screens = prompt('Какие типы экранов нужно разработать?', "Простые, Сложные, Интерактивные");
-let screenPrice = +prompt('Сколько будет стоить данная работа?' , '10000');
-let adaptive = confirm("Нужен ли адаптив на сайте?");
+const title = getTitle(prompt('как называется ваш проект?'));
+const screens = prompt('Какие типы экранов нужно разработать?', "Простые, Сложные, Интерактивные");
+const screenPrice = Number(prompt('Сколько будет стоить данная работа?'));
+const rollBack = Math.random(1, 100);
+const adaptive = Boolean(prompt("Нужен ли адаптив на сайте?"));
 
-let rollBack = 10 ;
-let allServicePrices 
-let fullPrice
-let servicePercentPrice
-
-
+const service1 = prompt('Какой дополнительный тип услуги нужен?', "service1, service2");
+const servicePrice1 = Number(prompt('Сколько это будет стоить?', "400, 800"));
+const service2 = prompt('Какой дополнительный тип услуги нужен?', "service1, service2");
+const servicePrice2 = Number(prompt('Сколько это будет стоить?', "400, 800"));
 
 
 
 
 
-
-
-
-const getAllServicePrices = function() {
-  // return num1 + num2 
-  let sum 
-
-  for (let i = 0; i < 2 ; i++){
-    sum += +prompt('Сколько это будет стоить?', "400, 800")
-  }
-
-  return sum
-}
 
 function getTitle(title) {
-  return title.charAt(0).toUpperCase() + 
-         title.slice(1).toLowerCase()
-} 
+    return title.charAt(0).toUpperCase() + 
+           title.slice(1).toLowerCase()
+}
+
+
+
+function getAllServicePrices(num1, num2) {
+  return num1 + num2
+}
+const allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
 
 
 function getFullPrice(num1, num2) {
   return num1 + num2
 }
+const fullPrice = getFullPrice(screenPrice, allServicePrices);
 
 function getServicePercentPrices(num1) {
   return num1 - 200;
 }
-
-title = getTitle(title)
-allServicePrices = getAllServicePrices();
-fullPrice = +getFullPrice(screenPrice, allServicePrices);
-servicePercentPrice = getServicePercentPrices(fullPrice);
+const servicePercentPrice = getServicePercentPrices(fullPrice);
 console.log(servicePercentPrice);
 
-console.log("allServicePrices " , allServicePrices)
-
-
-
-const getRollbackMessage = function(fullPrice){
-  if (fullPrice > 30000) {
-    console.log("Даем скидку в 10%");
-  } else if (fullPrice > 15000 || fullPrice < 30000 ) {
-    console.log("Даем скидку в 5%");
-  } else if (fullPrice > 0 || fullPrice < 15000) {
-    console.log("Скидка не предусмотрена(");
-  } else if(fullPrice < 0) {
-    console.log("Что-то пошло не так(");
-  } else{
-    console.log("ты как к этому пришел");
-  }
+if (fullPrice > 30000) {
+  console.log("Даем скидку в 10%");
+} else if (fullPrice > 15000 || fullPrice < 30000 ) {
+  console.log("Даем скидку в 5%");
+} else if (fullPrice > 0 || fullPrice < 15000) {
+  console.log("Скидка не предусмотрена(");
+} else if(fullPrice < 0) {
+  console.log("Что-то пошло не так(");
+} else{
+  console.log("ты как к этому пришел");
 }
-
-console.log(getRollbackMessage(fullPrice))
-
-console.log("Стоимость верстки экранов " + screenPrice + "рублей и Стоимость разработки сайта " + fullPrice + "рублей")
 // console.log(typeof(title));
 // console.log(typeof(fullPrice));
 // console.log(typeof(adaptive));
